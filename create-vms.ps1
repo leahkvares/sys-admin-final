29894a7b70a5a628a5b71e3ac4eb31e67d61dd43
Dave The Bear Man <143228428+DaveTheBearMan@users.noreply.github.com>
2024-11-19 19:18:22 -0500

    Update create-vms.ps1
    Added writing windows hosts

--- create-vms.ps1
+++ create-vms.ps1
@@ -5,11 +5,24 @@
 #>
 # Move to the VMrun directory
 Set-Location "C:\Program Files (x86)\VMware\VMware Workstation"
+$rocky="Rocky 9"
+$pfSense="pfSense-2.5.1"
+$windows="VLWindows_10_22h2"
 
 # Clone and start the rocky VM
-.\vmrun -T ws clone "C:\Virtual Machines\Rocky 9\Rocky 9.vmx" "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\Rocky 9.vmx" linked
-.\vmrun -T ws start "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\Rocky 9.vmx"
+Write-Host "Creating $($rocky) Virtual Machine"
+.\vmrun -T ws clone "C:\Virtual Machines\$($rocky)\$($rocky).vmx" "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\$($rocky).vmx" linked
+Write-Host "Starting $($rocky) Virtual Machine"
+.\vmrun -T ws start "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\$($rocky).vmx"
 
-# Clone and start the pfSense VM
-.\vmrun -T ws clone "C:\Virtual Machines\pfSense-2.5.1\pfSense-2.5.1.vmx" "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\pfSense-2.5.1.vmx" linked
-.\vmrun -T ws start "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\pfSense-2.5.1.vmx"
+# Clone and start the pfsense VM
+Write-Host "Creating $($pfSense) Virtual Machine"
+.\vmrun -T ws clone "C:\Virtual Machines\$($pfSense)\$($pfSense).vmx" "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\$($pfSense).vmx" linked
+Write-Host "Starting $($pfSense) Virtual Machine"
+.\vmrun -T ws start "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\$($pfSense).vmx"
+
+# Clone and start the windows VM
+Write-Host "Creating $($windows) Virtual Machine"
+.\vmrun -T ws clone "C:\Virtual Machines\$($windows)\$($windows).vmx" "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\$($windows).vmx" linked
+Write-Host "Starting $($windows) Virtual Machine"
+.\vmrun -T ws start "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\$($windows).vmx"
