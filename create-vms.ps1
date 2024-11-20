76fd69e6cd1ada3e1d4c655e8f19449c95d960e4
Dave The Bear Man <143228428+DaveTheBearMan@users.noreply.github.com>
2024-11-19 19:00:35 -0500

    Update create-vms.ps1
    Add pfsense automation

--- create-vms.ps1
+++ create-vms.ps1
@@ -7,5 +7,9 @@
 Set-Location "C:\Program Files (x86)\VMware\VMware Workstation"
 
 # Clone and start the rocky VM
-.\vmrun -T ws clone "C:\Virtual Machines\Rocky 9\Rocky 9.vmx" "C:\Users\GCCISAdmin\Documents\Virtual Machines\Rocky 9 Clone\Rocky 9.vmx" linked
-.\vmrun -T ws start "C:\Users\GCCISAdmin\Documents\Virtual Machines\Rocky 9 Clone\Rocky 9.vmx"
+.\vmrun -T ws clone "C:\Virtual Machines\Rocky 9\Rocky 9.vmx" "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\Rocky 9.vmx" linked
+.\vmrun -T ws start "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\Rocky 9.vmx"
+
+# Clone and start the pfSense VM
+.\vmrun -T ws clone "C:\Virtual Machines\pfSense-2.5.1\pfSense-2.5.1.vmx" "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\pfSense-2.5.1.vmx" linked
+.\vmrun -T ws start "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\pfSense-2.5.1.vmx"
