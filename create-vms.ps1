f00c61c94165aeba473a615fa27d3c11c8028770
Dave The Bear Man <143228428+DaveTheBearMan@users.noreply.github.com>
2024-11-19 19:34:24 -0500

    Update create-vms.ps1
    Now works for vm Name and vmx File

--- create-vms.ps1
+++ create-vms.ps1
@@ -7,13 +7,18 @@
 # Move to the VMrun directory
 Set-Location "C:\Program Files (x86)\VMware\VMware Workstation"
 
-# List of VMs to create and start
-$vmList = @("Rocky 9", "pfSense-2.5.1", "VLWindows_10_22h2")
+# List of VM directories and their associated .vmx file names
+$vmList = @(
+    @{ Name = "Rocky 9"; VmxFile = "Rocky 9.vmx" },
+    @{ Name = "pfSense-2.5.1"; VmxFile = "pfSense-2.5.1.vmx" },
+    @{ Name = "VLWindows_10_22h2"; VmxFile = "VLWindows_10_22h2.vmx" },
+    @{ Name = "WinServer2k22"; VmxFile = "Windows Server 2019.vmx" }  # Special case
+)
 
 # Loop through each VM in the list
 foreach ($vm in $vmList) {
-    Write-Host "Creating $vm Virtual Machine"
-    .\vmrun -T ws clone "C:\Virtual Machines\$vm\$vm.vmx" "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\$vm.vmx" linked
-    Write-Host "Starting $vm Virtual Machine"
-    .\vmrun -T ws start "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\$vm.vmx"
+    Write-Host "Creating $($vm.Name) Virtual Machine"
+    .\vmrun -T ws clone "C:\Virtual Machines\$($vm.Name)\$($vm.VmxFile)" "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\$($vm.Name).vmx" linked
+    Write-Host "Starting $($vm.Name) Virtual Machine"
+    .\vmrun -T ws start "C:\Users\GCCISAdmin\Documents\Virtual Machines\SysAdminFinal\$($vm.Name).vmx"
 }
