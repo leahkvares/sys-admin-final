d7889e9919d0d75f076bd21d0583e3bfa0d9d6ba
DaveTheBearMan <davethebearman@gmail.com>
2024-12-11 19:38:13 +0000

    Updated setup.yml for rockylinux to add AD

--- create-users.ps1
+++ create-users.ps1
@@ -9,7 +9,7 @@ Import-Module ActiveDirectory
 
 # Create User1 (Domain Users group member)
 Write-Host "Creating user 1..."
-New-ADUser -Name $User1Name -SamAccountName $User1Name -UserPrincipalName "$User1Name@example.com" `
+New-ADUser -Name $User1Name -SamAccountName $User1Name -UserPrincipalName "$User1Name@davidleah.com" `
     -Path $OU -AccountPassword $Password -Enabled $true -PasswordNeverExpires $true `
     -CannotChangePassword $true -ChangePasswordAtLogon $false
 Add-ADGroupMember -Identity "Domain Users" -Members $User1Name
@@ -17,7 +17,7 @@ Write-Host "User1 created successfully."
 
 # Create User2 (Domain Admins group member)
 Write-Host "Creating user 2..."
-New-ADUser -Name $User2Name -SamAccountName $User2Name -UserPrincipalName "$User2Name@example.com" `
+New-ADUser -Name $User2Name -SamAccountName $User2Name -UserPrincipalName "$User2Name@davidleah.com" `
     -Path $OU -AccountPassword $Password -Enabled $true -PasswordNeverExpires $true `
     -CannotChangePassword $true -ChangePasswordAtLogon $false
 Add-ADGroupMember -Identity "Domain Admins" -Members $User2Name
